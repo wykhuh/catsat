dc66c79bc019bba67d6edcf9d5ca19faf60b65c6
wykhuh <wykhuh@users.noreply.github.com>
2015-04-12 12:50:48 -0700

    css changes

---
+++ client/components/tagsbar/tagsbar.controller.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('spaceappApp')
+  .controller('NavbarCtrl', function ($scope, $location, Auth) {
+
+
+    $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
