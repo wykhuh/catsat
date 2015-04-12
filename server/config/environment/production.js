817589d5a05305bd042413dc826cc169aa9e453f
wykhuh <wykhuh@users.noreply.github.com>
2015-04-11 19:40:22 -0700

    mongo config

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/spaceapp'
+            "mongodb://IbmCloud_qlum225q_qqtdr8bu_7hsenrbu:D5SiXgiEtN2pEAzGWGXrFknPz0pAwCFV@ds055190.mongolab.com:55190/IbmCloud_qlum225q_qqtdr8bu?replicaSet=rs-ds055190"
   }
 };
